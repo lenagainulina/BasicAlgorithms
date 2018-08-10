e166e87261ec497b0f8dd7ee26e17e8ada2b9a7c
Lena <41228626+lenagainulina@users.noreply.github.com>
2018-08-10 23:46:21 +0200

    Update Main.java

--- ReverseLine/src/Main.java
+++ ReverseLine/src/Main.java
@@ -5,11 +5,21 @@ Reverse a string recursively
 public class Main {
     public static void main(String[] args) {
         String revStr = "SummeR";
-        System.out.println(reverse(revStr));
+        System.out.println(reverseRecursion(revStr));
+        System.out.println(reverseLoop(revStr));
     }
 
-    public static String reverse(String input) {
+    public static String reverseRecursion(String input) {
         if (input.length() <= 1) return input;
-        return reverse(input.substring(1)) + input.charAt(0);
+        return reverseRecursion(input.substring(1)) + input.charAt(0);
+    }
+
+    public static String reverseLoop(String str) {
+        String res = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            res += str.charAt(i);
+        }
+        return res;
     }
 }
